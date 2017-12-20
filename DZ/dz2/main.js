a2a51d8fe8c36a5bb66ef405ec20fb82107d654b
Ivan Prokopenko <web.prokopenko@gmail.com>
2017-12-20 15:47:45 +0200

    DZ2 bad

--- DZ/dz2/main.js
+++ DZ/dz2/main.js
@@ -1,19 +1,34 @@
 const dotenv = require('dotenv');
-const env = dotenv.config({path: './config.env'});
+const env = dotenv.config({ path: './config.env' });
+const http = require('http');
+const fs = require('fs');
+const dateFormat = require('dateformat');
+var now = new Date();
+
 
 if (env.error) {
-  throw env.error
+    throw env.error
 }
 
 const time = env.parsed.TIME;
 const interval = env.parsed.INTERVAL;
 
-let intervalId = setInterval(function() {
-    console.log("сет интервал");
-  }, interval);
 
-let timeoutId = setTimeout(function(){
-    console.log( 'Конец сет интервала' );
-    clearInterval(intervalId);
-  }, time);
+
+let  StartServer = function() {
+    server = http.createServer(function (request, response) {
+
+        let intervalId = setInterval(function () {
+            response.end(dateFormat(now, "longTime", true));
+            console.log(dateFormat(now, "longTime", true));
+        }, interval);
+
+        let timeoutId = setTimeout(function () {
+            clearInterval(intervalId);
+        }, time);
+
+    }).listen(3002);
+};
+
+StartServer();
 
